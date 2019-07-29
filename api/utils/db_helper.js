9de9281423183155e0a26600ac26a1529ba2f59b
VPBelo <venus.belo.rhi@gmail.com>
2019-07-29 14:39:39 +0800

    udpate

--- api/utils/db_helper.js
+++ api/utils/db_helper.js
@@ -3,7 +3,7 @@
 //DB connection
 // =============================================================================
 var mongoose = require('mongoose');
-var uri = 'mongodb://VPBelo:<password>@zoobicinn-shard-00-00-k0tbm.azure.mongodb.net:27017,zoobicinn-shard-00-01-k0tbm.azure.mongodb.net:27017,zoobicinn-shard-00-02-k0tbm.azure.mongodb.net:27017/test?ssl=true&replicaSet=ZoobicInn-shard-0&authSource=admin&retryWrites=true&w=majority';
+var uri = 'mongodb://VPBelo:<password>@zoobicinn-shard-00-00-k0tbm.azure.mongodb.net:27017,zoobicinn-shard-00-01-k0tbm.azure.mongodb.net:27017,zoobicinn-shard-00-02-k0tbm.azure.mongodb.net:27017/chatbot-template-sample?ssl=true&replicaSet=ZoobicInn-shard-0&authSource=admin&retryWrites=true&w=majority';
 
 mongoose.Promise = require('bluebird');
 
