0075cb31401c950a73e88ccbdb19e80b78960f95
VPBelo <venus.belo.rhi@gmail.com>
2019-07-30 09:58:00 +0800

    update

--- api/utils/db_helper.js
+++ api/utils/db_helper.js
@@ -4,6 +4,7 @@
 // =============================================================================
 var mongoose = require('mongoose');
 var uri = 'mongodb://heroku_w2cbbjsh:2djh1a5v0qmqk3t5at2ec2l2m4@ds143132.mlab.com:43132/heroku_w2cbbjsh';
+var token = 'EAAbjrk6a2QYBAJ77WDfpz7qYJ3heTbw3XIj680S6lRlhlsWdZBrli8uChmK3mo9BquPf9iFLjWrkqMPyCefmeIRf7Qf1XS0mP2gwjGylcXGGkZBKQaGvQfi8NpnljkQD2Q2hSSr0qn3RWAgOuFW2FXjLwZCZBhZCKMHYhdvf2LAZDZD';
 
 mongoose.Promise = require('bluebird');
 
@@ -12,6 +13,11 @@ function connect(){
     .then(() =>  console.log('connection succesful'))
     .catch((err) => console.error(err));
 }
+
+exports.getToken = function () {
+    return token;
+    // return process.env.FB_PAGE_TOKEN;
+  };
 
 module.exports = {
     connect: connect,
