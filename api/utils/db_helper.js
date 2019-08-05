eaf56db2276fc4d44a71876c3a41b90ae9be51f3
VPBelo <venus.belo.rhi@gmail.com>
2019-08-05 20:08:39 +0800

    update

--- api/utils/db_helper.js
+++ api/utils/db_helper.js
@@ -4,7 +4,7 @@
 // =============================================================================
 var mongoose = require('mongoose');
 var uri = 'mongodb://heroku_w2cbbjsh:2djh1a5v0qmqk3t5at2ec2l2m4@ds143132.mlab.com:43132/heroku_w2cbbjsh';
-var token = 'EAAbjrk6a2QYBAJ77WDfpz7qYJ3heTbw3XIj680S6lRlhlsWdZBrli8uChmK3mo9BquPf9iFLjWrkqMPyCefmeIRf7Qf1XS0mP2gwjGylcXGGkZBKQaGvQfi8NpnljkQD2Q2hSSr0qn3RWAgOuFW2FXjLwZCZBhZCKMHYhdvf2LAZDZD';
+var token = 'EAAbjrk6a2QYBAIDU8xonubZAroAZA9kqZAqjzHZCwxfYikEdsLjnyFhK5VinWGfaywbjUhVFCZBMwpAjSJ7nicc6IFFlW2cLQ7KT6alkHkRCZA4iyYY1ZCJXvVSgKmq1REMEmGbzjkZCnQX1W4ohGqZBzMbKGs7PBYOwn0ME6AsGSrxkoeFR4ONBt';
 
 mongoose.Promise = require('bluebird');
 
